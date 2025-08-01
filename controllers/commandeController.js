9d30a990aae08b00eea311ee062989e39bfc1c32
Athanase Kpan <athanasesura1@gmail.com>
2025-08-01 08:49:46 -0400

    first commit

--- controllers/commandeController.js
+++ controllers/commandeController.js
@@ -1,4 +1,4 @@
-import { transporter } from "../utils/mailService.js";
+import { transporter } from "../utils/mailService";
 
 export const traiterCommande = async (req, res) => {
   const { infosClient, produits, total } = req.body;
--- routes/commande.js
+++ routes/commande.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { traiterCommande } from "../controllers/commandeController.js";
+import { traiterCommande } from "../controllers/commandeController";
+
 
 const router = express.Router();
 
